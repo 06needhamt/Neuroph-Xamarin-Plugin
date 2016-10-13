8abe4922a6f8b39d089570f0922f6e0a6346686f
Tom Needham <06needhamt@gmail.com>
2016-10-13 17:06:05 +0100

    Init Project

---
+++ neuroph_xamarin_plugin/neuroph_xamarin_plugin/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"neuroph_xamarin_plugin",
+	Namespace = "neuroph_xamarin_plugin",
+	Version = "0.1"
+)]
+
+[assembly: AddinName("Neuroph-For-Xamarin")]
+[assembly: AddinCategory("Language Bindings")]
+[assembly: AddinDescription("Xamarin Studio plugin which allows for easy integration of the Neuroph c# bindings into Xamarin Studio and easy to use examples")]
+[assembly: AddinAuthor("Thomas Needham")]
